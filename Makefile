98df59ffa658d4ca5282c8dfa25758bfd7187f66
Andreas Bissinger <mail@bissinger-andreas.de>
2016-02-25 13:51:54 +0100

    settings

---
+++ Makefile
@@ -0,0 +1,3 @@
+run:
+	mvn package
+	java -cp target/WebShop-1.0-SNAPSHOT.jar dhbw.cdn.shop.App
--- src/main/java/dhbw/cdn/shop/App.java
+++ src/main/java/dhbw/cdn/shop/App.java
@@ -1,3 +1,5 @@
+package dhbw.cdn.shop;
+
 public class App
 {
     public static void main(String[] args) {
